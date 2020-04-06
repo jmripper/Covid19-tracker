ca2e8551e62bf10c30c29cbea3fb4ea09ec192d8
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-04-05 20:15:13 -0400

    Updates

---
+++ precache-manifest.256000de7e63bab7799f92acaaee02e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0306c2e5ee0aed58efc7a4237e2294",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "5447de79a2ef85d7c77a",
+    "url": "/covid19-tracker/static/css/main.f4d4c7f1.chunk.css"
+  },
+  {
+    "revision": "33b5a847e821bf7374de",
+    "url": "/covid19-tracker/static/js/2.8b176a14.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid19-tracker/static/js/2.8b176a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5447de79a2ef85d7c77a",
+    "url": "/covid19-tracker/static/js/main.c2816f58.chunk.js"
+  },
+  {
+    "revision": "49b718abe4b605019d95",
+    "url": "/covid19-tracker/static/js/runtime-main.e792bcae.js"
+  }
+]);
